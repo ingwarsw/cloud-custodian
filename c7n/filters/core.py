5fbd0f8d904cd592cc178ea8a84f137e3a26eafb
Karol Lassak <ingwar@ingwar.eu.org>
2019-12-27 18:45:45 +0100

    Allow regexps with value-from

--- c7n/filters/core.py
+++ c7n/filters/core.py
@@ -449,7 +449,7 @@ class ValueFilter(Filter):
             if not self.data['op'] in OPERATORS:
                 raise PolicyValidationError(
                     "Invalid operator in value filter %s" % self.data)
-            if self.data['op'] in {'regex', 'regex-case'}:
+            if self.data['op'] in {'regex', 'regex-case'} and self.data.get('value'):
                 # Sanity check that we can compile
                 try:
                     re.compile(self.data['value'])
